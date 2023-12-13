097ef3a799deb713408ac48b28970fae12d50bd5
chhavitekriwal <chhavi.tekriwal@gmail.com>
2023-12-14 04:27:49 +0530

    fix: updateDB function to work with axios

--- utils/updateDB.js
+++ utils/updateDB.js
@@ -3,15 +3,13 @@ const {Coin} = require("../models/coin.model")
 
 const updateDB = async () => {
     try {
-        const coins = await axios.get("https://api.coingecko.com/api/v3/coins/list");
+        const coins = (await axios.get("https://api.coingecko.com/api/v3/coins/list")).data;
         for (const coin of coins){
             await Coin.updateOne({id:coin.id},coin,{upsert: true});
         }
         console.log("Database Updated");
     } catch (error) {
-        console.error("Error updating database:");
-        console.error("Error Name:", error.name);
-        console.error("Error Message:", error.message);
+        console.error("Error updating database.");
     }
 }
 module.exports = updateDB
