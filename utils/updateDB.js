aa82c906bb1169972654bc4a627781186e4a58c1
chhavitekriwal <chhavi.tekriwal@gmail.com>
2023-12-14 18:56:22 +0530

    prettified code;

--- utils/updateDB.js
+++ utils/updateDB.js
@@ -7,18 +7,18 @@ const updateDB = async () => {
     logger.info('Starting database update');
     const fetchedCoins = (await axios.get('https://api.coingecko.com/api/v3/coins/list')).data;
     const existingCoins = await Coin.find({}, {id: 1, _id: 0});
-    
+
     const fetchedCoinIds = new Set(fetchedCoins.map(coin => coin.id));
     const existingCoinIds = new Set(existingCoins.map(coin => coin.id));
 
-    const coinsToAdd = fetchedCoins.filter(coin => !(existingCoinIds.has(coin.id)));
-    const coinsToDelete = existingCoins.filter(coin => !(fetchedCoinIds.has(coin.id))).map(coin => coin.id);
+    const coinsToAdd = fetchedCoins.filter(coin => !existingCoinIds.has(coin.id));
+    const coinsToDelete = existingCoins.filter(coin => !fetchedCoinIds.has(coin.id)).map(coin => coin.id);
 
     if (coinsToAdd.length > 0) {
       await Coin.insertMany(coinsToAdd);
     }
     if (coinsToDelete.length > 0) {
-      await Coin.deleteMany({id : {$in: coinsToDelete}});
+      await Coin.deleteMany({id: {$in: coinsToDelete}});
     }
     const coins = await Coin.countDocuments();
     logger.info(`Database Updated. ${coins} records.`);
