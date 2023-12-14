5e01d742775f9d9c26898be24be0e17a47f5cf10
chhavitekriwal <chhavi.tekriwal@gmail.com>
2023-12-14 18:01:35 +0530

    fix: delete stale records from DB;

--- utils/updateDB.js
+++ utils/updateDB.js
@@ -6,16 +6,22 @@ const updateDB = async () => {
   try {
     logger.info('Starting database update');
     const fetchedCoins = (await axios.get('https://api.coingecko.com/api/v3/coins/list')).data;
-
     const existingCoins = await Coin.find({}, {id: 1, _id: 0});
+    
+    const fetchedCoinIds = new Set(fetchedCoins.map(coin => coin.id));
     const existingCoinIds = new Set(existingCoins.map(coin => coin.id));
 
-    const newCoins = fetchedCoins.filter(coin => !(existingCoinIds.has(coin.id)));
+    const coinsToAdd = fetchedCoins.filter(coin => !(existingCoinIds.has(coin.id)));
+    const coinsToDelete = existingCoins.filter(coin => !(fetchedCoinIds.has(coin.id))).map(coin => coin.id);
 
-    if (newCoins.length > 0) {
-      await Coin.insertMany(newCoins);
+    if (coinsToAdd.length > 0) {
+      await Coin.insertMany(coinsToAdd);
     }
-    logger.info('Database Updated');
+    if (coinsToDelete.length > 0) {
+      const result = await Coin.deleteMany({id : {$in: coinsToDelete}});
+    }
+    const coins = await Coin.countDocuments();
+    logger.info(`Database Updated. ${coins} records.`);
   } catch (error) {
     logger.error('Error updating database');
     logger.error(error);
