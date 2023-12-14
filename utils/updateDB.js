72f2487a5fcf761ab7ff67fccaed36b0395998e4
chhavitekriwal <chhavi.tekriwal@gmail.com>
2023-12-14 13:25:21 +0530

    optimize update of database;

--- utils/updateDB.js
+++ utils/updateDB.js
@@ -1,13 +1,21 @@
 const axios = require('axios');
 const Coin = require('../models/coin.model');
+const logger = require('../utils/winston');
 
 const updateDB = async () => {
   try {
-    const coins = (await axios.get('https://api.coingecko.com/api/v3/coins/list')).data;
-    for (const coin of coins) {
-      await Coin.updateOne({id: coin.id}, coin, {upsert: true});
+    logger.info('Starting database update');
+    const fetchedCoins = (await axios.get('https://api.coingecko.com/api/v3/coins/list')).data;
+
+    const existingCoins = await Coin.find({}, {id: 1, _id: 0});
+    const existingCoinIds = new Set(existingCoins.map(coin => coin.id));
+
+    const newCoins = fetchedCoins.filter(coin => !(existingCoinIds.has(coin.id)));
+
+    if (newCoins.length > 0) {
+      await Coin.insertMany(newCoins);
     }
-    console.log('Database Updated');
+    logger.info('Database Updated');
   } catch (error) {
     logger.error('Error updating database');
     logger.error(error);
