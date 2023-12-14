e9e733d2e628fa5f06d8938332bda95ae2e1a9cf
chhavitekriwal <chhavi.tekriwal@gmail.com>
2023-12-15 00:36:06 +0530

    handle uncaught exceptions;

--- index.js
+++ index.js
@@ -4,10 +4,38 @@ dotenv.config();
 const app = require('./app');
 const logger = require('./utils/winston');
 
+const PORT = process.env.PORT || 8080;
 let server;
-mongoose.connect(process.env.MONGO_URI).then(() => {
-  logger.info('Connected to database');
-  server = app.listen(process.env.PORT, () => {
-    logger.info(`Listening to port ${process.env.PORT}`);
-  });
+mongoose.connect(process.env.MONGO_URI)
+        .then(()=>{
+          logger.info("Connected to MongoDB database")
+          server = app.listen(PORT, ()=>{
+            logger.info(`Server listening on ${PORT}`)
+          });
+        })
+
+const exitHandler = () => {
+  if (server) {
+    server.close(() => {
+      logger.info('Server closed');
+      process.exit(1);
+    });
+  } else {
+    process.exit(1);
+  }
+};
+
+const unexpectedErrorHandler = (error) => {
+  logger.error(error);
+  exitHandler();
+};
+
+process.on('uncaughtException', unexpectedErrorHandler);
+process.on('unhandledRejection', unexpectedErrorHandler);
+
+process.on('SIGTERM', () => {
+  logger.info('SIGTERM received');
+  if (server) {
+    server.close();
+  }
 });
